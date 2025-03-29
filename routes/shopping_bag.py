06c81195b5e6f0f8bec55d0eb4878c5432525da9
borislavvoytash <bvoytash@gmail.com>
2025-03-29 18:19:54 +0200

    endpoints to put item in bag and get list of items in bag, which are stored in cookies

--- routes/shopping_bag.py
+++ routes/shopping_bag.py
@@ -7,6 +7,7 @@ from models.users import UserProfile
 from routes.auth import auth_user_dependency
 from validators.shopping_bag import create_bag_item_dependency,update_bag_item_dependency, create_shopping_bag_dependency
 import json
+from os import getenv
 
 router = APIRouter(
     prefix="/shopping_bag",
@@ -16,69 +17,53 @@ router = APIRouter(
 COOKIE_NAME = "guest_shopping_bag"
 
 
-@router.post("/add_item", status_code=status.HTTP_201_CREATED)
+@router.post("/add_item", status_code=201)
 async def add_item_to_bag(
-    request: create_bag_item_dependency,
+    request: Request,
+    request_dependency: create_bag_item_dependency,
     response: Response,
 ):
-    cookie_data = response.cookies.get(COOKIE_NAME)
+    
+    cookie_data = request.cookies.get(COOKIE_NAME)
+    COOKIE_MAX_AGE = int(getenv("COOKIE_MAX_AGE"))
+    items = []
+
     if cookie_data:
         try:
             items = json.loads(cookie_data)
         except json.JSONDecodeError:
-            items = []
-    else:
-        items = []
+            pass
 
     for item in items:
-        if item["product_id"] == request.product_id and item.get("size") == request.size:
-            item["quantity"] += request.quantity
+        if item["product_id"] == request_dependency.product_id and item.get("size") == request_dependency.size:
+            item["quantity"] += request_dependency.quantity
             item["total_price"] = item["price"] * item["quantity"]
             break
     else:
         items.append({
-            "product_id": request.product_id,
-            "product_name": request.product_name,
-            "quantity": request.quantity,
-            "price": request.price,
-            "size": request.size,
-            "total_price": request.price * request.quantity
+            "product_id": request_dependency.product_id,
+            "product_name": request_dependency.product_name,
+            "quantity": request_dependency.quantity,
+            "price": request_dependency.price,
+            "size": request_dependency.size,
+            "total_price": request_dependency.price * request_dependency.quantity
         })
 
-    response.set_cookie(key=COOKIE_NAME, value=json.dumps(items), httponly=True)
-    return JSONResponse(
-        content={"shopping_bag_items": items},
-        status_code=status.HTTP_201_CREATED
-    )
+    response.set_cookie(
+        COOKIE_NAME,  
+        json.dumps(items),  
+        max_age=COOKIE_MAX_AGE,  
+        path="/",  
+        domain=None,
+        secure=True,  
+        httponly=True,  
+        samesite="Strict",
+)
+
+    response.status_code = status.HTTP_201_CREATED
+    response.content = json.dumps({"shopping_bag_items": items})
+    return JSONResponse
     
-
-
-@router.delete("/remove_item", status_code=status.HTTP_200_OK)
-async def remove_item_from_bag(
-    product_id: int,
-    size: str,
-    response: Response,
-):
-
-    cookie_data = response.cookies.get(COOKIE_NAME)
-
-    if not cookie_data:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No items found in the shopping bag")
-    try:
-        items = json.loads(cookie_data)
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cookie data")
-
-
-    updated_items = [item for item in items if not (item["product_id"] == product_id and item["size"] == size)]
-    
-    response.set_cookie(key=COOKIE_NAME, value=json.dumps(updated_items), httponly=True)
-
-    return JSONResponse(
-        content={"detail": "Item removed successfully"},
-        status_code=status.HTTP_200_OK
-    )
-
 
 @router.get("/items", status_code=status.HTTP_200_OK)
 async def get_shopping_bag_items(
@@ -86,12 +71,13 @@ async def get_shopping_bag_items(
 ):
 
     cookie_data = request.cookies.get(COOKIE_NAME)
-
+    print(cookie_data)
     if not cookie_data:
         return {"items": []}
 
     try:
         items = json.loads(cookie_data)
+        print(items)
         return JSONResponse(
             content={"items": items},
             status_code=status.HTTP_200_OK
@@ -99,55 +85,4 @@ async def get_shopping_bag_items(
     except json.JSONDecodeError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cookie data")
     
-
-@router.post("/checkout", status_code=status.HTTP_201_CREATED)
-async def checkout_shopping_bag(
-    user_profile_id: int,
-    request: Request,
-    response: Response,
-    db: db_dependency,
-    auth_user_dependency: auth_user_dependency
-):
-
-    cookie_data = request.cookies.get(COOKIE_NAME)
-
-    if not cookie_data:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Shopping bag is empty")
-
-    try:
-        items = json.loads(cookie_data)
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cookie data")
-
-
-    user_profile = db.query(UserProfile).filter(UserProfile.id == user_profile_id).first()
-    
-    if not user_profile:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User profile not found")
-
-    new_shopping_bag = ShoppingBag()
-    db.add(new_shopping_bag)
-    db.commit()
-
-    for item in items:
-        new_item = BagItem(
-            bag_id=new_shopping_bag.id,
-            product_id=item["product_id"],
-            product_name=item["product_name"],
-            quantity=item["quantity"],
-            price=item["price"],
-            size=item.get("size"),
-            total_price=item["total_price"]
-        )
-        db.add(new_item)
-
-    db.commit()
-
-    response.delete_cookie(COOKIE_NAME)
-
-    return JSONResponse(
-        content={"detail":"Shopping bag successfully checked out"},
-        status_code=status.HTTP_201_CREATED
-    )
-
 
