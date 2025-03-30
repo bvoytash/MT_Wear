1f3b607c88e2ce492036bcf588a88b2830df25cd
borislavvoytash <bvoytash@gmail.com>
2025-03-30 10:50:01 +0300

    remove item from bag endpoint added

--- routes/shopping_bag.py
+++ routes/shopping_bag.py
@@ -74,7 +74,6 @@ async def get_shopping_bag_items(
 ):
 
     cookie_data = request.cookies.get(COOKIE_NAME)
-    print(cookie_data)
     if not cookie_data:
         return {"items": []}
 
@@ -86,5 +85,54 @@ async def get_shopping_bag_items(
         )
     except json.JSONDecodeError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cookie data")
+
+
+@router.post("/items/remove", status_code=status.HTTP_200_OK)
+async def remove_bag_item(
+    # crsf_token: csrf_dependency,
+    request: Request,
+    response: Response,
+    request_body: dict
+):
+    cookie_data = request.cookies.get(COOKIE_NAME)
+    if not cookie_data:
+        return JSONResponse(content={"message": "Shopping bag is empty"}, status_code=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        items = json.loads(cookie_data)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cookie data")
+
+    product_id = request_body.get("product_id")
+    size = request_body.get("size")
+    updated_items = []
+    item_removed = False
+
+    for item in items:
+        if item["product_id"] == product_id and item.get("size") == size:
+            if item["quantity"] > 1:
+                item["quantity"] -= 1
+                item["total_price"] = item["price"] * item["quantity"]
+                updated_items.append(item)
+            item_removed = True
+        else:
+            updated_items.append(item)
+    
+    if not item_removed:
+        return JSONResponse(content={"message": "Item not found in bag"}, status_code=status.HTTP_404_NOT_FOUND)
+    
+    response.set_cookie(
+        COOKIE_NAME,
+        json.dumps(updated_items),
+        max_age=int(getenv("COOKIE_MAX_AGE")),
+        path="/",
+        domain=None,
+        secure=True,
+        httponly=True,
+        samesite="Strict",
+    )
+
+    return JSONResponse
+
     
 
