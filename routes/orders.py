37d6f04e71fadbb28ee2c5d27ee17862902c1c91
borislavvoytash <bvoytash@gmail.com>
2025-04-03 19:19:04 +0300

    create endpoint to GET orders by user ID

--- routes/orders.py
+++ routes/orders.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, status, Request
 from models.orders import Order, OrderStatus, BagItem
 from models.users import UserProfile
 import json
-from routes.auth import csrf_dependency, auth_user_dependency
+from routes.auth import csrf_dependency, auth_user_dependency, auth_admin_dependency
 from database import db_dependency
 from validators.order import create_order_dependency
 from validators.shopping_bag import create_bag_item_dependency
@@ -11,6 +11,47 @@ from models.orders import Order
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 COOKIE_NAME = "guest_shopping_bag"
+
+
+
+@router.get("/{user_id}", status_code=status.HTTP_200_OK)
+async def get_order_by_user_id (
+    user_id: int, db: db_dependency,
+    auth_user_dependency: auth_user_dependency,
+    ):
+
+     user_profile = db.query(UserProfile).filter_by(user_id=user_id).first()
+
+     if not user_profile:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User profile not found."
+        )
+     
+     orders = db.query(Order).filter_by(user_profile_id=user_profile.id).all()
+
+     if not orders:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No orders found for this user."
+        )
+        
+     orders_response = [
+        {
+            "id": order.id,
+            "order_number": order.order_number,
+            "created": order.created,
+            "is_paid": order.is_paid,
+            "status": order.status.value,
+            "total_price": order.total_price,
+        }
+        for order in orders
+    ]
+     return {"orders": orders_response}
+
+
+
+
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_order(
@@ -78,7 +119,9 @@ async def create_order(
 
 
 @router.get("/all_orders", status_code=status.HTTP_200_OK)
-async def get_all_orders(db: db_dependency):
+async def get_all_orders(db: db_dependency,
+                         auth_admin_dependency,
+                         ):
     orders = db.query(Order).all()
     if not orders:
         raise HTTPException(
